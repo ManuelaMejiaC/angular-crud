2844f62c100291611e92ad49b1c07caef33bd725
Manuela <manuelamcastano@gmail.com>
2022-09-22 17:58:44 -0500

    Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crud';
-}
+
+
+  employeeArray: Employee[] = [
+    {id: 1, name: 'Alejandro', country: 'Colombia'},
+    {id: 2, name: 'Manuela', country: 'Venezuela'},
+    {id: 3, name: 'Andrea', country: 'Estados Unidos'}
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }else
+    this.selectedEmployee = new Employee();
+  }
+  delete(){
+    if(confirm('Are you sure you want to delete?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+    }
+  }
+} 
